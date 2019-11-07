da8bd4f003393f7a7b42feaa8ac8b9be8451f012
anthony-roux <roux.anthony@hetic.net>
2019-11-07 22:32:40 +0100

    Added design style images

---
+++ javascript.js
@@ -0,0 +1,7 @@
+const motDePass = ("Vera Farmiga")
+motDePass = prompt("Qui joue le role de Norma Bates dans la série d'anthologie Bates Motel ?");
+if (motDePass === "Vera Farmiga") {
+  console.log("C'est exacte ! Bravo !");
+} else if (motDePass !== "Vera Farmiga") {
+  console.log("Non, ce n'est pas ça... si ça peut t'aider, elle joue le role de la méchante, dans 'La None'");
+}
